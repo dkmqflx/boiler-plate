f3d964e6176d16e7cd7a6978e5ee50ae63669765
dkmqflx <dkmqflx@gmail.com>
2023-12-09 22:15:34 +0900

    chore: add react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import QueryClientProvider from './QueryProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +16,13 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
+    
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <QueryClientProvider>
+        {children}
+        </QueryClientProvider>
+        </body>
     </html>
   )
 }
